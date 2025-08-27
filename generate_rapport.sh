4d5999f7d9206a9b82a41a5043a8c083818d08fe
Gael NOKAM NOKAM <nokamnokamgael@gmail.com>
2025-08-27 17:08:54 +0200

    tr

--- generate_rapport.sh
+++ generate_rapport.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 npm ci
 npx playwright install --with-deps
-npx playwright test --reporter=html
-set PLAYWRIGHT_JUNIT_OUTPUT_NAME=results.xml
-npx playwright test --reporter=junit
+npx playwright test 
