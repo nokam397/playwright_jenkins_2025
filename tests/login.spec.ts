8f32ed9e00579c56858d1d37e1aec7a012fc63c8
Gael NOKAM NOKAM <nokamnokamgael@gmail.com>
2025-08-27 14:59:52 +0200

    rt

---
+++ tests/login.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+import LoginPage from '../pages/LoginPage';
+
+test('username correct et pass correct',{tag:['@tc-001', '@smoke',"@regression"]}, async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  const loginPage:LoginPage= new LoginPage(page);
+  await loginPage.saisirUsername('standard_user');
+  await loginPage.saisirPassword('secret_sauce');
+  await loginPage.clicSurLogin();
+  await expect(page.url()).toBe('https://www.saucedemo.com/inventory.html'); 
+  
+});
+
+test('username incorrect et pass correct',{tag:['@tc-002', '@smoke',"@regression"]}, async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  const loginPage:LoginPage= new LoginPage(page);
+  await loginPage.saisirUsername('standard_user_incorrect');
+  await loginPage.saisirPassword('secret_sauce');
+  await loginPage.clicSurLogin();
+  await expect( await loginPage.getErrorMessage()).toBeVisible();
+});
+
